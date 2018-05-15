fdc7096c7fd449ec17b08be913f8b1fb2b2ff6ef
sketchycode <jeremy@sketchyventures.com>
2018-05-15 14:33:17 -0600

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aad_mfa_poc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
